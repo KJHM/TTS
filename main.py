fbb5b6c2ebbc59ce3116a8989d030616eb153dbf
Hee-Min Ju <ggark99@gmail.com>
2021-11-06 08:05:43 +0900

    - implement input-data Importing - implement UI
    (※ Notice) 이 프로젝트와 같은 디렉토리에 mlp-singer 프로젝트가 위치한다고 가정, 아닐 시 line 132, line 150의 data/raw 폴더를 가리키는 상대경로를 수정해야 합니다
    
    ├── ...
    │   └── ...
    │       ├── TTS (this project)
    │       └── mlp-singer
    
    (※ Notice) load_singleFile 혹은 load_directory에서 파일을 선택하지 않고 그냥 ok를 눌러버리면 프로그램이 죽습니다... (급해서 일단 예외처리 생략함...)

--- main.py
+++ main.py
@@ -3,45 +3,167 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QBasicTimer
 
-class MyApp(QWidget):
+import os
+from os import path
+import shutil
 
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+class LogStringHandler(logging.Handler):
+    def __init__(self, target_widget):
+        super(LogStringHandler, self).__init__()
+        self.target_widget = target_widget
+
+    def emit(self, record):
+        self.target_widget.append(record.asctime + ' -- ' + record.getMessage())
+
+class MyApp(QWidget):
     def __init__(self):
         super().__init__()
         self.initWidget()
 
     def initWidget(self):
-        #Button
-        self.AButton = QPushButton('Import input Data')
-        self.BButton = QPushButton('Model Learning')
-        self.CButton = QPushButton('Produce TTS')
+        widgetGrid = QGridLayout()
+        widgetGrid.addWidget(self.createInputDataImportGroup(), 0, 0)
+        widgetGrid.addWidget(self.createModelLearningGroup(), 1, 0)
+        widgetGrid.addWidget(self.createProduceTTSGroup(), 2, 0)
 
-        #TODO: log 관련
-
-        #TODO: button action AButton
-
-        #TODO: button action BButton
-
-        #TODO: button action CButton
-
-
-        #button layout
+        loggroup = QGroupBox('Event Log')
+        self.loggingtext = QTextBrowser()
         hbox = QHBoxLayout()
-        hbox.addStretch(1)
-        hbox.addWidget(self.AButton)
-        hbox.addWidget(self.BButton)
-        hbox.addWidget(self.CButton)
-        hbox.addStretch(1)
+        hbox.addWidget(self.loggingtext)
+        loggroup.setLayout(hbox)
 
-        vbox = QVBoxLayout()
-        vbox.addStretch(1)
-        vbox.addLayout(hbox)
-        vbox.addStretch(1)
-
-        self.setLayout(vbox)
-
+        grid = QGridLayout()
+        grid.addLayout(widgetGrid, 0, 0)
+        grid.addWidget(loggroup, 0, 1)
+        self.setLayout(grid)
 
         self.setWindowTitle('MPSinger_Widget')
+        self.setGeometry(300, 300, 480, 320)
         self.show()
+
+        logger = logging.getLogger()
+        logger.addHandler(LogStringHandler(self.loggingtext))
+
+
+    def createInputDataImportGroup(self):
+        groupbox = QGroupBox('Import Input Data')
+
+        self.smode = QRadioButton('By single file')
+        self.dmode = QRadioButton('By directory')
+        self.smode.setChecked(True)
+        self.AButton = QPushButton('Select')
+        self.AButton.clicked.connect(self.inputDataImport)
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.smode)
+        hbox.addStretch(1)
+        hbox.addWidget(self.dmode)
+        hbox.addStretch(3)
+
+        grid = QGridLayout()
+        grid.addLayout(hbox, 0, 0)
+        grid.addWidget(self.AButton, 0, 1)
+        groupbox.setLayout(grid)
+
+        return groupbox
+
+    def createModelLearningGroup(self):
+        groupbox = QGroupBox('Model Learning')
+
+        self.pbar = QProgressBar(self)
+        self.BButton = QPushButton('Learn')
+        # TODO: button action BButton
+
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.pbar)
+
+        grid = QGridLayout()
+        grid.addLayout(hbox, 0, 0)
+        grid.addWidget(self.BButton, 0, 1)
+        groupbox.setLayout(grid)
+
+        return groupbox
+
+    def createProduceTTSGroup(self):
+        groupbox = QGroupBox('Produce TTS')
+
+        self.Dummybtn = QPushButton(' ')
+        self.Dummybtn.setFlat(True)
+        self.CButton = QPushButton('Produce')
+        # TODO: button action CButton
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.Dummybtn)
+        hbox.addStretch(3)
+
+        grid = QGridLayout()
+        grid.addLayout(hbox, 0, 0)
+        grid.addWidget(self.CButton, 0, 1)
+        groupbox.setLayout(grid)
+
+        return groupbox
+
+    def inputDataImport(self):
+        reply = QMessageBox.question(self, 'Warning', 'This action will\nremove or overwrite files. continue?',
+                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            if self.smode.isChecked():
+                self.load_singleFile('mid')
+                self.load_singleFile('txt')
+                self.load_singleFile('wav')
+            elif self.dmode.isChecked():
+                self.load_directory('mid')
+                self.load_directory('txt')
+                self.load_directory('wav')
+
+        else:
+            print("data import rejected...")
+
+    def load_singleFile(self, file_format):
+        title = 'Open ' + file_format
+        ffilter = file_format + ' File(*.' + file_format + ');; All File(*)'    #1-1. get source file path
+        fname = QFileDialog.getOpenFileName(self, title, './', ffilter)
+
+        if fname[0] != "":
+            copy = r'..\mlp-singer\data\raw'            #1-2. get target file path
+            copy = copy + '\\' + file_format
+            existing_files = os.listdir(copy)           #2. remove existing files
+            logging.info('removed files...')
+            for file in existing_files:
+                if path.exists(copy + '\\' + file):
+                    os.unlink(copy + '\\' + file)
+                    logging.info('\t' + file)
+
+            shutil.copy(fname[0], copy)                 #3. load src to target
+            logging.info('loaded files...')
+            logging.info('\t' + os.path.basename(fname[0]))
+        else:
+            logging.info('skip loading ' + file_format + ' file')
+
+    def load_directory(self, file_format):
+        origin, ok = QInputDialog.getText(self, 'Input Path', file_format + " directory path:")   #1-1. get source directory path
+        if ok:
+            copy = r'..\mlp-singer\data\raw'            #1-2. get target directory path
+            copy = copy + '\\' + file_format
+            existing_files = os.listdir(copy)           #2. remove existing files
+            logging.info('removed files...')
+            for file in existing_files:
+                if path.exists(copy + '\\' + file):
+                    os.unlink(copy + '\\' + file)
+                    logging.info('\t' + file)
+
+            files = os.listdir(origin)                  #3. load src to target
+            logging.info('loaded files...')
+            for file in files:
+                if not path.exists(copy + '\\' + file):
+                    shutil.copy(origin + '\\' + file, copy + '\\' + file)
+                    logging.info('\t' + file)
+        else:
+            logging.info('skip loading ' + file_format + ' directory')
 
 
 class MyMainWindow(QMainWindow):
@@ -76,13 +198,9 @@ class MyMainWindow(QMainWindow):
 
         #Window Attribute
         self.setWindowTitle('MPSinger')
-        self.setGeometry(300, 300, 600, 600)
+        #self.setGeometry(300, 300, 600, 600)
+        self.setGeometry(300, 300, 960, 320)    #compact version
         self.show()
-
-
-
-
-
 
 
 if __name__ == '__main__':
